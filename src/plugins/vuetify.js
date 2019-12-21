b87d6ab29e9b5079d5216a0f376adc456c0cd89e
akikokomura <akikokomura@akikokomuranoMacBook-Pro.local>
2019-12-21 23:01:29 +0900

    色を変えてみる

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,4 +1,21 @@
+import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-const opts = {}
-export default new Vuetify(opts);
+Vue.use(Vuetify);
+export default new Vuetify(
+    {theme: {
+    themes: {
+      light: {
+        primary: '#e91e63',
+        secondary: '#9c27b0',
+        accent: '#673ab7',
+        error: '#3f51b5',
+        warning: '#2196f3',
+        info: '#03a9f4',
+        success: '#00bcd4'
+
+      },
+    },
+  },
+});
+
